a37e11678fb80b61a3c005d462e1a4a134380712
jaydip1253 <jaydhavale2306@gmail.com>
2025-01-03 12:30:24 +0530

    commit

---
+++ jay_school/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Jay_School',
+    'version' : '1.2',
+    'summary': 'School Management',
+    'description': """ School Management """,
+    'category': '',
+    'website': '',
+    'depends' : [],
+    'data': [],
+    'license': 'LGPL-3',
+}
